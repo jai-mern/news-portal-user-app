166e4630481b7f0e50ed745190826c87474c7d5b
jai-mern <Kjaya8862@gamail.com>
2023-12-27 07:08:00 +0530

    add bootstrap

--- src/App.js
+++ src/App.js
@@ -7,8 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          jaihind 
         </p>
+        <h1>welcome jaihind</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
